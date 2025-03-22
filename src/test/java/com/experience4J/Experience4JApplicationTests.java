ed28e1b6d05a6d848550dc75a34c088272d1441f
Pauline Turk <pauline.turk@ext.bpifrance.fr>
2025-03-22 09:48:14 +0100

    feat: first commit

---
+++ src/test/java/com/experience4J/Experience4JApplicationTests.java
@@ -0,0 +1,13 @@
+package com.experience4J;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Experience4JApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
